b6d371487f893ecb74ba78c1f53c763b4d9731b9
Ash Huston <anthony.huston@covenanteyes.com>
2025-06-12 09:06:18 -0400

    Added Comment

--- prisonerBots/ASH_bot.py
+++ prisonerBots/ASH_bot.py
@@ -9,6 +9,7 @@ myBot: BotData = {
 
 def getAction(currentMatchHistory: list[MatchResult]) -> bool:
 # This strategy is simple. We start with cooperating, then we just do whatever the opponent did last round.
+# Strategies could be much more complex.
     willCooperate: bool = True
     if len(currentMatchHistory) > 0:
         willCooperate = currentMatchHistory[-1][myBot["oppAssignment"]]["cooperated"]
