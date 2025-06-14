46841790d636a6a23c35a39f67620146be65e9d0
Ash Huston <anthony.huston@covenanteyes.com>
2025-06-14 15:51:00 -0400

    demo wip

--- prisonerBots/ASH_bot.py
+++ prisonerBots/ASH_bot.py
@@ -1,17 +1,19 @@
-from main import MatchResult, BotData
+from main import MatchResult
+from bot import Bot
 
-myBot: BotData = {
-    "submitterName": "Ash Huston",
-    "botName": "Tit for Tat",
-    "assignment":  "",
-    "oppAssignment": "",
-}
-
-def getAction(currentMatchHistory: list[MatchResult]) -> bool:
+def funct(currentMatchHistory: list[MatchResult]) -> bool:
 # This strategy is simple. We start with cooperating, then we just do whatever the opponent did last round.
-# Strategies could be much more complex.
+# Strategies could be much more complex. Maybe even simpler.
     willCooperate: bool = True
     if len(currentMatchHistory) > 0:
         willCooperate = currentMatchHistory[-1][myBot["oppAssignment"]]["cooperated"]
-
+    print(willCooperate)
     return willCooperate
+
+myBot = Bot(
+    "Ash Huston",
+    "Tit for Tat",
+    funct
+)
+
+print(myBot.getAction([]))
--- prisonerBots/main.py
+++ prisonerBots/main.py
@@ -1,3 +1,5 @@
+import os
+from bot import Bot
 
 class BotResult:
     cooperated: bool
@@ -7,8 +9,40 @@ class MatchResult:
     botA: BotResult
     botB: BotResult
 
-class BotData:
-    submitterName: str
-    botName: str
-    assignment: str
-    oppAssignment: str
+def getOutputFilePath():
+    def ensureOutputFile():
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(current_dir, "output.txt")
+        if not os.path.exists(file_path):
+            with open(file_path, 'w') as f:
+                pass  # just create the file
+        return file_path
+    return ensureOutputFile()
+        
+outputFile = getOutputFilePath()
+
+def simulateRound(actionA: bool, actionB: bool):
+    results = [0, 0]
+    match [actionA, actionB]:
+        case [True, True]:
+            results = [3,3]
+        case [False, False]:
+            results = [1,1]
+        case [True, False]:
+            results = [0,5]
+        case [False, True]:
+            results = [5,0]
+    return results
+
+def simulateMatch(botA: Bot, botB: Bot, totalRounds: int):
+    matchHistory = []
+    for i in range(totalRounds):
+        botAAction = botA.getAction(),
+        botBAction = botB.getAction(),
+        roundResults = simulateRound(botAAction, botBAction)
+        thisRound = {
+            "botAAction": botAAction,
+            "botBAction": botBAction,
+            "botAScore": roundResults[0],
+            "botBScore":roundResults[1],
+        }
